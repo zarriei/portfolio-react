b3f21606068a6227c7f17d52a21248aea55d5e3a
zarriei <75182441+zarriei@users.noreply.github.com>
2021-05-25 16:38:38 -0400

    src base

---
+++ src/resumeData.js
@@ -0,0 +1,75 @@
+let resumeData = {
+  name: "Eiman Zarrinhonar",
+  role: "Full Stack MERN Student",
+  linkedinId: "Eiman Zarrinhonar",
+  roleDescription:
+    "I like learning about full stack developing and the various new technologies.  In my free time I highly enjoy traveling and soaking in other cultures and ways of life .",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/eiman-zarrinhonar/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/zarriei",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "I am currently a student of Full Stack Development at Georgia Institute of Technology.  I highly enjoy to travel and learn from various cultures and soak in other ways of life from all corners of the world. I enjoy spending time with my family.  I am a movie and car fanatic.  ",
+  address: "United States",
+  website: "https://github.com/zarriei",
+  education: [
+    {
+      UniversityName: "Kennesaw State University",
+      specialization: "Marketing",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2018",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Verizon",
+      specialization: "Global Enterprise Advisor",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+    },
+  ],
+  skillsDescription: "Proficient Languages",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+};
+
+export default resumeData;
